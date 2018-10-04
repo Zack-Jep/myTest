225301daba952cea79d2bbc3c0289f5d0d56fc4c
Jacob Adlers <jacob.adlers@gmail.com>
2018-10-04 20:51:25 +0200

    Run yarn fmt:format

--- src/main/resources/webassets/spec/spec-helper.js
+++ src/main/resources/webassets/spec/spec-helper.js
@@ -3,17 +3,18 @@
  * Author: Rasmus Ros, rasmus.ros@cs.lth.se
  */
 var specHelper = (function() {
-    // Fetch a HTML file and add it to a new div, calling 'done' when finished. Done is obtained from jasmine to
-    // block until completion.
-    var fetchHtml = function(url, el) {
-        var node = document.createElement('div');
-        el.appendChild(node);
-        return fetch(url).then(response => response.text())
-            .then(function(text) {
-                node.innerHTML = text;
-                return node;
-            });
-    };
+  // Fetch a HTML file and add it to a new div, calling 'done' when finished. Done is obtained from jasmine to
+  // block until completion.
+  var fetchHtml = function(url, el) {
+    var node = document.createElement('div');
+    el.appendChild(node);
+    return fetch(url)
+      .then(response => response.text())
+      .then(function(text) {
+        node.innerHTML = text;
+        return node;
+      });
+  };
 
-    return {fetchHtml};
+  return { fetchHtml };
 })();
