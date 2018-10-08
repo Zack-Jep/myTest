3b4291f535bf0a818e7c17a965d3c480c60a0fb9
Andreas Welander <andreas.welander@mac.com>
2018-10-08 22:54:42 +0200

    Add backend support for registration of new user
    Adds backend support for registration of new user, provided information of user name, password and role. Must be updated to accept email and omit role, and to check availability of user credentials.

--- src/main/java/se/lth/base/server/rest/UserResource.java
+++ src/main/java/se/lth/base/server/rest/UserResource.java
@@ -19,120 +19,128 @@ import se.lth.base.server.data.*;
 @Path("user")
 public class UserResource {
 
-  public static final String USER_TOKEN = "USER_TOKEN";
+	public static final String USER_TOKEN = "USER_TOKEN";
 
-  private final ContainerRequestContext context;
-  private final User user;
-  private final Session session;
-  private final UserDataAccess userDao = new UserDataAccess(Config.instance().getDatabaseDriver());
+	private final ContainerRequestContext context;
+	private final User user;
+	private final Session session;
+	private final UserDataAccess userDao = new UserDataAccess(Config.instance().getDatabaseDriver());
 
-  public UserResource(@Context ContainerRequestContext context) {
-    this.context = context;
-    this.user = (User) context.getProperty(User.class.getSimpleName());
-    this.session = (Session) context.getProperty(Session.class.getSimpleName());
-  }
+	public UserResource(@Context ContainerRequestContext context) {
+		this.context = context;
+		this.user = (User) context.getProperty(User.class.getSimpleName());
+		this.session = (Session) context.getProperty(Session.class.getSimpleName());
+	}
 
-  @GET
-  @PermitAll
-  @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-  public User currentUser() {
-    return user;
-  }
+	@GET
+	@PermitAll
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public User currentUser() {
+		return user;
+	}
 
-  @Path("login")
-  @POST
-  @PermitAll
-  @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
-  public Response login(
-      Credentials credentials, @QueryParam("remember") @DefaultValue("false") boolean rememberMe)
-      throws URISyntaxException {
-    Session newSession = userDao.authenticate(credentials);
-    int maxAge = rememberMe ? (int) TimeUnit.DAYS.toSeconds(7) : NewCookie.DEFAULT_MAX_AGE;
-    return Response.noContent()
-        .cookie(newCookie(newSession.getSessionId().toString(), maxAge, null))
-        .build();
-  }
+	@Path("login")
+	@POST
+	@PermitAll
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Response login(Credentials credentials, @QueryParam("remember") @DefaultValue("false") boolean rememberMe)
+			throws URISyntaxException {
+		Session newSession = userDao.authenticate(credentials);
+		int maxAge = rememberMe ? (int) TimeUnit.DAYS.toSeconds(7) : NewCookie.DEFAULT_MAX_AGE;
+		return Response.noContent().cookie(newCookie(newSession.getSessionId().toString(), maxAge, null)).build();
+	}
 
-  private NewCookie newCookie(String value, int maxAge, Date expiry) {
-    return new NewCookie(
-        USER_TOKEN,
-        value, // value
-        "/rest", // path
-        context.getUriInfo().getBaseUri().getHost(), // host
-        NewCookie.DEFAULT_VERSION, // version
-        "", // comment
-        maxAge, // max-age
-        expiry, // expiry
-        false, // secure
-        true); // http-onle
-  }
+	private NewCookie newCookie(String value, int maxAge, Date expiry) {
+		return new NewCookie(USER_TOKEN, value, // value
+				"/rest", // path
+				context.getUriInfo().getBaseUri().getHost(), // host
+				NewCookie.DEFAULT_VERSION, // version
+				"", // comment
+				maxAge, // max-age
+				expiry, // expiry
+				false, // secure
+				true); // http-onle
+	}
 
-  @Path("logout")
-  @POST
-  @PermitAll
-  public Response logout() {
-    userDao.removeSession(session.getSessionId());
-    return Response.noContent().cookie(newCookie("", 0, new Date(0L))).build();
-  }
+	@Path("logout")
+	@POST
+	@PermitAll
+	public Response logout() {
+		userDao.removeSession(session.getSessionId());
+		return Response.noContent().cookie(newCookie("", 0, new Date(0L))).build();
+	}
 
-  @Path("roles")
-  @GET
-  @RolesAllowed(Role.Names.ADMIN)
-  @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-  public Set<Role> getRoles() {
-    return Role.ALL_ROLES;
-  }
+	@Path("roles")
+	@GET
+	@RolesAllowed(Role.Names.ADMIN)
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public Set<Role> getRoles() {
+		return Role.ALL_ROLES;
+	}
 
-  @POST
-  @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
-  @RolesAllowed(Role.Names.ADMIN)
-  public User createUser(Credentials credentials) {
-    if (!credentials.hasPassword() || !credentials.validPassword()) {
-      throw new WebApplicationException("Password too short", Response.Status.BAD_REQUEST);
-    }
-    return userDao.addUser(credentials);
-  }
+	// Credentials need to be updated to accept email and have Role optional. 
+	// Check to see if email or user name is available. 
+	@Path("register")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@PermitAll
+	public User registerUser(Credentials credentials) {
+		if (!credentials.hasPassword() || !credentials.validPassword()) {
+			throw new WebApplicationException("Password too short", Response.Status.BAD_REQUEST);
+		}
+		return userDao.addUser(credentials);
+	}
 
-  @Path("all")
-  @GET
-  @RolesAllowed(Role.Names.ADMIN)
-  @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-  public List<User> getUsers() {
-    return userDao.getUsers();
-  }
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@RolesAllowed(Role.Names.ADMIN)
+	public User createUser(Credentials credentials) {
+		if (!credentials.hasPassword() || !credentials.validPassword()) {
+			throw new WebApplicationException("Password too short", Response.Status.BAD_REQUEST);
+		}
+		return userDao.addUser(credentials);
+	}
 
-  @Path("{id}")
-  @GET
-  @RolesAllowed(Role.Names.ADMIN)
-  @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-  public User getUser(@PathParam("id") int userId) {
-    return userDao.getUser(userId);
-  }
+	@Path("all")
+	@GET
+	@RolesAllowed(Role.Names.ADMIN)
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public List<User> getUsers() {
+		return userDao.getUsers();
+	}
 
-  @Path("{id}")
-  @RolesAllowed(Role.Names.ADMIN)
-  @PUT
-  @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
-  @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
-  public User putUser(@PathParam("id") int userId, Credentials credentials) {
-    if (credentials.hasPassword() && !credentials.validPassword()) {
-      throw new WebApplicationException("Password too short", Response.Status.BAD_REQUEST);
-    }
-    if (userId == user.getId() && user.getRole().getLevel() > credentials.getRole().getLevel()) {
-      throw new WebApplicationException("Cant't demote yourself", Response.Status.BAD_REQUEST);
-    }
-    return userDao.updateUser(userId, credentials);
-  }
+	@Path("{id}")
+	@GET
+	@RolesAllowed(Role.Names.ADMIN)
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public User getUser(@PathParam("id") int userId) {
+		return userDao.getUser(userId);
+	}
 
-  @Path("{id}")
-  @RolesAllowed(Role.Names.ADMIN)
-  @DELETE
-  public void deleteUser(@PathParam("id") int userId) {
-    if (userId == currentUser().getId()) {
-      throw new WebApplicationException("Don't delete yourself", Response.Status.BAD_REQUEST);
-    }
-    if (!userDao.deleteUser(userId)) {
-      throw new WebApplicationException("User not found", Response.Status.NOT_FOUND);
-    }
-  }
+	@Path("{id}")
+	@RolesAllowed(Role.Names.ADMIN)
+	@PUT
+	@Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	@Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
+	public User putUser(@PathParam("id") int userId, Credentials credentials) {
+		if (credentials.hasPassword() && !credentials.validPassword()) {
+			throw new WebApplicationException("Password too short", Response.Status.BAD_REQUEST);
+		}
+		if (userId == user.getId() && user.getRole().getLevel() > credentials.getRole().getLevel()) {
+			throw new WebApplicationException("Cant't demote yourself", Response.Status.BAD_REQUEST);
+		}
+		return userDao.updateUser(userId, credentials);
+	}
+
+	@Path("{id}")
+	@RolesAllowed(Role.Names.ADMIN)
+	@DELETE
+	public void deleteUser(@PathParam("id") int userId) {
+		if (userId == currentUser().getId()) {
+			throw new WebApplicationException("Don't delete yourself", Response.Status.BAD_REQUEST);
+		}
+		if (!userDao.deleteUser(userId)) {
+			throw new WebApplicationException("User not found", Response.Status.NOT_FOUND);
+		}
+	}
 }
--- src/test/java/se/lth/base/server/rest/UserResourceTest.java
+++ src/test/java/se/lth/base/server/rest/UserResourceTest.java
@@ -19,190 +19,182 @@ import se.lth.base.server.data.User;
 
 public class UserResourceTest extends BaseResourceTest {
 
-  private static final GenericType<List<User>> USER_LIST = new GenericType<List<User>>() {};
+	private static final GenericType<List<User>> USER_LIST = new GenericType<List<User>>() {
+	};
 
-  @Test
-  public void notAuthenticatedCurrentUser() {
-    User user = target("user").request().get(User.class);
-    assertEquals(Role.NONE, user.getRole());
-  }
+	@Test
+	public void notAuthenticatedCurrentUser() {
+		User user = target("user").request().get(User.class);
+		assertEquals(Role.NONE, user.getRole());
+	}
 
-  @Test(expected = ForbiddenException.class)
-  public void notAuthenticatedGetAllUsers() {
-    target("user").path("all").request().get(USER_LIST);
-  }
+	@Test(expected = ForbiddenException.class)
+	public void notAuthenticatedGetAllUsers() {
+		target("user").path("all").request().get(USER_LIST);
+	}
 
-  @Test
-  public void loginCookies() {
-    Response response = target("user").path("login").request().post(Entity.json(TEST_CREDENTIALS));
-    Cookie responseCookie = response.getCookies().get(UserResource.USER_TOKEN);
-    assertEquals("localhost", responseCookie.getDomain());
-    assertEquals(UserResource.USER_TOKEN, responseCookie.getName());
-    assertEquals("/rest", responseCookie.getPath());
+	@Test
+	public void loginCookies() {
+		Response response = target("user").path("login").request().post(Entity.json(TEST_CREDENTIALS));
+		Cookie responseCookie = response.getCookies().get(UserResource.USER_TOKEN);
+		assertEquals("localhost", responseCookie.getDomain());
+		assertEquals(UserResource.USER_TOKEN, responseCookie.getName());
+		assertEquals("/rest", responseCookie.getPath());
 
-    User userWithNoCookie = target("user").request().get(User.class);
-    assertEquals(Role.NONE, userWithNoCookie.getRole());
+		User userWithNoCookie = target("user").request().get(User.class);
+		assertEquals(Role.NONE, userWithNoCookie.getRole());
 
-    User userWithCookie = target("user").request().cookie(responseCookie).get(User.class);
-    assertEquals(Role.USER, userWithCookie.getRole());
-  }
+		User userWithCookie = target("user").request().cookie(responseCookie).get(User.class);
+		assertEquals(Role.USER, userWithCookie.getRole());
+	}
 
-  @Test
-  public void loginRememberMeCookie() {
-    Response loginWithRememberMe =
-        target("user")
-            .path("login")
-            .queryParam("remember", "true")
-            .request()
-            .post(Entity.json(TEST_CREDENTIALS));
-    int maxAge = loginWithRememberMe.getCookies().get(UserResource.USER_TOKEN).getMaxAge();
-    assertTrue(maxAge > 0);
+	@Test
+	public void loginRememberMeCookie() {
+		Response loginWithRememberMe = target("user").path("login").queryParam("remember", "true").request()
+				.post(Entity.json(TEST_CREDENTIALS));
+		int maxAge = loginWithRememberMe.getCookies().get(UserResource.USER_TOKEN).getMaxAge();
+		assertTrue(maxAge > 0);
 
-    Response loginWithoutRememberMe =
-        target("user").path("login").request().post(Entity.json(TEST_CREDENTIALS));
-    int noMaxAge = loginWithoutRememberMe.getCookies().get(UserResource.USER_TOKEN).getMaxAge();
-    assertEquals(-1, noMaxAge);
-  }
+		Response loginWithoutRememberMe = target("user").path("login").request().post(Entity.json(TEST_CREDENTIALS));
+		int noMaxAge = loginWithoutRememberMe.getCookies().get(UserResource.USER_TOKEN).getMaxAge();
+		assertEquals(-1, noMaxAge);
+	}
 
-  @Test
-  public void logout() {
-    Response noSessionLogout = target("user").path("logout").request().post(Entity.json(""));
-    assertEquals("", noSessionLogout.getCookies().get(UserResource.USER_TOKEN).getValue());
+	@Test
+	public void logout() {
+		Response noSessionLogout = target("user").path("logout").request().post(Entity.json(""));
+		assertEquals("", noSessionLogout.getCookies().get(UserResource.USER_TOKEN).getValue());
 
-    Response loginResponse =
-        target("user").path("login").request().post(Entity.json(ADMIN_CREDENTIALS));
-    assertFalse(loginResponse.getCookies().get(UserResource.USER_TOKEN).getValue().isEmpty());
+		Response loginResponse = target("user").path("login").request().post(Entity.json(ADMIN_CREDENTIALS));
+		assertFalse(loginResponse.getCookies().get(UserResource.USER_TOKEN).getValue().isEmpty());
 
-    Response logoutResponse =
-        target("user")
-            .path("logout")
-            .request()
-            .cookie(loginResponse.getCookies().get(UserResource.USER_TOKEN))
-            .post(Entity.json(""));
-    assertTrue(logoutResponse.getCookies().get(UserResource.USER_TOKEN).getValue().isEmpty());
+		Response logoutResponse = target("user").path("logout").request()
+				.cookie(loginResponse.getCookies().get(UserResource.USER_TOKEN)).post(Entity.json(""));
+		assertTrue(logoutResponse.getCookies().get(UserResource.USER_TOKEN).getValue().isEmpty());
 
-    User currentUserAfterLogout = target("user").request().get(User.class);
-    assertEquals(Role.NONE, currentUserAfterLogout.getRole());
-  }
+		User currentUserAfterLogout = target("user").request().get(User.class);
+		assertEquals(Role.NONE, currentUserAfterLogout.getRole());
+	}
 
-  @Test(expected = ForbiddenException.class)
-  public void getAllUsersAsUser() {
-    login(TEST_CREDENTIALS);
-    target("user").path("all").request().get(USER_LIST);
-  }
+	@Test(expected = ForbiddenException.class)
+	public void getAllUsersAsUser() {
+		login(TEST_CREDENTIALS);
+		target("user").path("all").request().get(USER_LIST);
+	}
 
-  @Test(expected = ForbiddenException.class)
-  public void getUserAsUser() {
-    login(TEST_CREDENTIALS);
-    target("user").path(Integer.toString(ADMIN.getId())).request().get(User.class);
-  }
+	@Test(expected = ForbiddenException.class)
+	public void getUserAsUser() {
+		login(TEST_CREDENTIALS);
+		target("user").path(Integer.toString(ADMIN.getId())).request().get(User.class);
+	}
 
-  @Test(expected = ForbiddenException.class)
-  public void createUserAsUser() {
-    login(TEST_CREDENTIALS);
-    target("user")
-        .request()
-        .post(Entity.json(""), Void.class); // Include response type to trigger exception
-  }
+	@Test(expected = ForbiddenException.class)
+	public void createUserAsUser() {
+		login(TEST_CREDENTIALS);
+		target("user").request().post(Entity.json(""), Void.class); // Include response type to trigger exception
+	}
 
-  @Test(expected = ForbiddenException.class)
-  public void deleteUserAsUser() {
-    login(TEST_CREDENTIALS);
-    target("user")
-        .path(Integer.toString(ADMIN.getId()))
-        .request()
-        .delete(Void.class); // Include response type to trigger exception
-  }
+	@Test(expected = ForbiddenException.class)
+	public void deleteUserAsUser() {
+		login(TEST_CREDENTIALS);
+		target("user").path(Integer.toString(ADMIN.getId())).request().delete(Void.class); // Include response type to
+																							// trigger exception
+	}
 
-  @Test
-  public void getAllUsers() {
-    login(ADMIN_CREDENTIALS);
-    List<User> users = target("user").path("all").request().get(USER_LIST);
-    assertTrue(users.stream().mapToInt(User::getId).anyMatch(id -> id == ADMIN.getId()));
-    assertTrue(users.stream().mapToInt(User::getId).anyMatch(id -> id == TEST.getId()));
-  }
+	@Test
+	public void getAllUsers() {
+		login(ADMIN_CREDENTIALS);
+		List<User> users = target("user").path("all").request().get(USER_LIST);
+		assertTrue(users.stream().mapToInt(User::getId).anyMatch(id -> id == ADMIN.getId()));
+		assertTrue(users.stream().mapToInt(User::getId).anyMatch(id -> id == TEST.getId()));
+	}
 
-  @Test
-  public void getRoles() {
-    login(ADMIN_CREDENTIALS);
-    Set<Role> roles = target("user").path("roles").request().get(new GenericType<Set<Role>>() {});
-    assertEquals(Role.ALL_ROLES, roles);
-  }
+	@Test
+	public void getRoles() {
+		login(ADMIN_CREDENTIALS);
+		Set<Role> roles = target("user").path("roles").request().get(new GenericType<Set<Role>>() {
+		});
+		assertEquals(Role.ALL_ROLES, roles);
+	}
 
-  @Test
-  public void testAddUser() {
-    login(ADMIN_CREDENTIALS);
-    Credentials newCredentials = new Credentials("pelle", "passphrase", Role.USER);
-    User newUser = target("user").request().post(Entity.json(newCredentials), User.class);
-    assertEquals(newCredentials.getUsername(), newUser.getName());
-    assertEquals(newCredentials.getRole(), newUser.getRole());
-    assertTrue(newUser.getId() > 0);
+	// Credentials need to be updated to accept email and have Role optional.
+	@Test
+	public void testRegisterUser() {
+		Credentials newCredentials = new Credentials("pelle", "passphrase", Role.USER);
+		User newUser = target("user").path("register").request().post(Entity.json(newCredentials), User.class);
+		assertEquals(newCredentials.getUsername(), newUser.getName());
+		assertEquals(newCredentials.getRole(), newUser.getRole());
+		assertTrue(newUser.getId() > 0);
 
-    // Test if we can login as new user
-    login(newCredentials);
-    User currentUser = target("user").request().get(User.class);
-    assertEquals(newUser.getId(), currentUser.getId());
-  }
+		// Test if we can login as new user
+		login(newCredentials);
+		User currentUser = target("user").request().get(User.class);
+		assertEquals(newUser.getId(), currentUser.getId());
+	}
 
-  @Test
-  public void getUser() {
-    login(ADMIN_CREDENTIALS);
-    User responseTest =
-        target("user").path(Integer.toString(TEST.getId())).request().get(User.class);
-    assertEquals(TEST.getId(), responseTest.getId());
-    assertEquals(TEST.getName(), responseTest.getName());
-    assertEquals(TEST.getRole(), responseTest.getRole());
-  }
+	@Test
+	public void testAddUser() {
+		login(ADMIN_CREDENTIALS);
+		Credentials newCredentials = new Credentials("pelle", "passphrase", Role.USER);
+		User newUser = target("user").request().post(Entity.json(newCredentials), User.class);
+		assertEquals(newCredentials.getUsername(), newUser.getName());
+		assertEquals(newCredentials.getRole(), newUser.getRole());
+		assertTrue(newUser.getId() > 0);
 
-  @Test(expected = WebApplicationException.class)
-  public void dontDeleteYourself() {
-    login(ADMIN_CREDENTIALS);
-    target("user").path(Integer.toString(ADMIN.getId())).request().delete(Void.class);
-  }
+		// Test if we can login as new user
+		login(newCredentials);
+		User currentUser = target("user").request().get(User.class);
+		assertEquals(newUser.getId(), currentUser.getId());
+	}
 
-  @Test(expected = NotFoundException.class)
-  public void deleteTestUser() {
-    login(ADMIN_CREDENTIALS);
-    target("user").path(Integer.toString(TEST.getId())).request().delete(Void.class);
-    target("user").path(Integer.toString(TEST.getId())).request().get(User.class);
-  }
+	@Test
+	public void getUser() {
+		login(ADMIN_CREDENTIALS);
+		User responseTest = target("user").path(Integer.toString(TEST.getId())).request().get(User.class);
+		assertEquals(TEST.getId(), responseTest.getId());
+		assertEquals(TEST.getName(), responseTest.getName());
+		assertEquals(TEST.getRole(), responseTest.getRole());
+	}
 
-  @Test(expected = NotFoundException.class)
-  public void deleteMissing() {
-    login(ADMIN_CREDENTIALS);
-    target("user").path(Integer.toString(-1)).request().delete(Void.class);
-  }
+	@Test(expected = WebApplicationException.class)
+	public void dontDeleteYourself() {
+		login(ADMIN_CREDENTIALS);
+		target("user").path(Integer.toString(ADMIN.getId())).request().delete(Void.class);
+	}
 
-  @Test(expected = NotFoundException.class)
-  public void updateMissing() {
-    login(ADMIN_CREDENTIALS);
-    target("user")
-        .path(Integer.toString(-1))
-        .request()
-        .put(Entity.json(TEST_CREDENTIALS), User.class);
-  }
+	@Test(expected = NotFoundException.class)
+	public void deleteTestUser() {
+		login(ADMIN_CREDENTIALS);
+		target("user").path(Integer.toString(TEST.getId())).request().delete(Void.class);
+		target("user").path(Integer.toString(TEST.getId())).request().get(User.class);
+	}
 
-  @Test(expected = WebApplicationException.class)
-  public void dontDemoteYourself() {
-    login(ADMIN_CREDENTIALS);
-    Credentials update = new Credentials("admin", "password", Role.USER);
-    target("user")
-        .path(Integer.toString(ADMIN.getId()))
-        .request()
-        .put(Entity.json(update), User.class);
-  }
+	@Test(expected = NotFoundException.class)
+	public void deleteMissing() {
+		login(ADMIN_CREDENTIALS);
+		target("user").path(Integer.toString(-1)).request().delete(Void.class);
+	}
 
-  @Test
-  public void updateUser() {
-    login(ADMIN_CREDENTIALS);
-    Credentials newTest = new Credentials("test2", null, Role.ADMIN);
-    User user =
-        target("user")
-            .path(Integer.toString(TEST.getId()))
-            .request()
-            .put(Entity.json(newTest), User.class);
-    assertEquals(TEST.getId(), user.getId());
-    assertEquals(newTest.getUsername(), user.getName());
-    assertEquals(newTest.getRole(), user.getRole());
-  }
+	@Test(expected = NotFoundException.class)
+	public void updateMissing() {
+		login(ADMIN_CREDENTIALS);
+		target("user").path(Integer.toString(-1)).request().put(Entity.json(TEST_CREDENTIALS), User.class);
+	}
+
+	@Test(expected = WebApplicationException.class)
+	public void dontDemoteYourself() {
+		login(ADMIN_CREDENTIALS);
+		Credentials update = new Credentials("admin", "password", Role.USER);
+		target("user").path(Integer.toString(ADMIN.getId())).request().put(Entity.json(update), User.class);
+	}
+
+	@Test
+	public void updateUser() {
+		login(ADMIN_CREDENTIALS);
+		Credentials newTest = new Credentials("test2", null, Role.ADMIN);
+		User user = target("user").path(Integer.toString(TEST.getId())).request().put(Entity.json(newTest), User.class);
+		assertEquals(TEST.getId(), user.getId());
+		assertEquals(newTest.getUsername(), user.getName());
+		assertEquals(newTest.getRole(), user.getRole());
+	}
 }
