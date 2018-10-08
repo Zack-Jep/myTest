c5c92048f8576200b69873fbe348a93864f0aba9
look4jesper <jesper.lundqvist12@gmail.com>
2018-10-08 13:48:45 +0200

    Added method co2Saved

--- src/main/java/se/lth/base/server/data/Trip.java
+++ src/main/java/se/lth/base/server/data/Trip.java
@@ -1,5 +1,25 @@
 package se.lth.base.server.data;
 
-public class Trip {
+import java.awt.Point;
+import java.time.LocalDateTime;
+import java.util.List;
 
+public class Trip {
+	
+	private LocalDateTime departureTime;
+	private LocalDateTime expectedTime;
+	private int capacity;
+	private Point departureAdress;
+	private final static int CO2PERKM = 120;
+	private List<User> passengers;
+	private double distance;
+	
+	/** Calculates the total CO2 saved on a trip.
+	 * The used value for CO2PERKM is 120g/km, taken from http://ecoscore.be/en/info/ecoscore/co2
+	 * @return Total CO2 saved
+	 *  */
+	public double co2Saved() {
+		return (CO2PERKM * distance) * passengers.size();
+	}
+	
 }
